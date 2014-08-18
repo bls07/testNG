b61ed4c6a0a8e49f615fa3f2b2a622a11a1870fa
srinivasulu <srinivasulu@digital-nirvana.com>
2014-08-18 17:26:31 +0530

    again a timeOut

--- Module_TestNG_ANT/src/tests/LoginTest113.java
+++ Module_TestNG_ANT/src/tests/LoginTest113.java
@@ -34,7 +34,7 @@ public class LoginTest113 {
   
   
 	
-	@Test()
+	@Test(timeOut=3000)
 	public void login113()  {
 		driver.findElement(By.id("user_login")).clear();
 	    driver.findElement(By.id("user_login")).sendKeys("admin");
