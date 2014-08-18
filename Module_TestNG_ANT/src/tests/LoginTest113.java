ef4be5acc40fe88aa8ddfb94e5eb97d914d4a535
srinivasulu <srinivasulu@digital-nirvana.com>
2014-08-18 16:46:54 +0530

    new push

--- Module_TestNG_ANT/src/tests/LoginTest113.java
+++ Module_TestNG_ANT/src/tests/LoginTest113.java
@@ -40,7 +40,7 @@ public class LoginTest113 {
 	    driver.findElement(By.id("user_login")).sendKeys("admin");
 	    driver.findElement(By.id("user_pass")).sendKeys("admin589");
 		driver.findElement(By.cssSelector("input.btn.login-btn")).click();
-		System.out.println("workspace change");
+		System.out.println("workspace change 2");
  }
 	
 	@Test(dependsOnMethods = { "login113" })
