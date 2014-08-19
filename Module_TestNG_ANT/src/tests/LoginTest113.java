c5cad7904fd789aee55c4c00cffd5ac1070d87b6
srinivasulu <srinivasulu@digital-nirvana.com>
2014-08-19 12:53:11 +0530

    f 2

--- Module_TestNG_ANT/src/tests/LoginTest113.java
+++ Module_TestNG_ANT/src/tests/LoginTest113.java
@@ -40,7 +40,7 @@ public class LoginTest113 {
 	    driver.findElement(By.id("user_login")).sendKeys("admin");
 	    driver.findElement(By.id("user_pass")).sendKeys("admin589");
 		driver.findElement(By.cssSelector("input.btn.login-btn")).click();
-		System.out.println("frequent commit 1");
+		System.out.println("frequent commit 2");
  }
 	
 	@Test(dependsOnMethods = { "login113" })
